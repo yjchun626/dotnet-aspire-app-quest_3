895fecb17efe3d26b5f661e229bb52ed46227c9d
yj_chun <yjchun626@gmail.com>
2024-07-30 17:01:30 +0900

    Replace AOAI Settings from AppApi to AppHost

--- workshop/AspireYouTubeSummariser.AppHost/Program.cs
+++ workshop/AspireYouTubeSummariser.AppHost/Program.cs
@@ -1,6 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
-var apiapp = builder.AddProject<Projects.AspireYouTubeSummariser_ApiApp>("apiapp");
+var config = builder.Configuration;
 var cache = builder.AddRedis("cache");
+var apiapp = builder.AddProject<Projects.AspireYouTubeSummariser_ApiApp>("apiapp")
+                    .WithEnvironment("OpenAI__Endpoint", config["OpenAI:Endpoint"])
+                    .WithEnvironment("OpenAI__ApiKey", config["OpenAI:ApiKey"])
+                    .WithEnvironment("OpenAI__DeploymentName", config["OpenAI:DeploymentName"]);
 
 builder.AddProject<Projects.AspireYouTubeSummariser_WebApp>("webapp")
        .WithReference(cache)
