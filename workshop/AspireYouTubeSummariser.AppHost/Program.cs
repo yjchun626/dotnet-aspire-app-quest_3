6236a4b613b091e48abcc62017bae6c5fef92f4a
yj_chun <yjchun626@gmail.com>
2024-07-31 15:02:01 +0900

    Add External Endpoints on ApiApp

--- workshop/AspireYouTubeSummariser.AppHost/Program.cs
+++ workshop/AspireYouTubeSummariser.AppHost/Program.cs
@@ -4,7 +4,8 @@ var cache = builder.AddRedis("cache");
 var apiapp = builder.AddProject<Projects.AspireYouTubeSummariser_ApiApp>("apiapp")
                     .WithEnvironment("OpenAI__Endpoint", config["OpenAI:Endpoint"])
                     .WithEnvironment("OpenAI__ApiKey", config["OpenAI:ApiKey"])
-                    .WithEnvironment("OpenAI__DeploymentName", config["OpenAI:DeploymentName"]);
+                    .WithEnvironment("OpenAI__DeploymentName", config["OpenAI:DeploymentName"])
+                    .WithExternalHttpEndpoints();
 
 builder.AddProject<Projects.AspireYouTubeSummariser_WebApp>("webapp")
        .WithExternalHttpEndpoints()
